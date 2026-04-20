9e49ba029180555f54a592890421aff853260400
Josshua David <2shuadavi@gmail.com>
2026-04-20 16:54:57 -0600

    Creacion de clases

---
+++ src/Modelo/Client.java
@@ -0,0 +1,9 @@
+package Modelo;
+
+public class Client extends Users {
+
+	public Client(String name, int age, int idUser, int CP, String email, String phone, String password) {
+		super(name, age, idUser, CP, email, phone, password);
+	}
+
+}
